4ec875d2595b7fcc7175e4d878205fc7344a7c76
Mayank Busa <mayankbusa111@gamil.com>
2025-05-23 09:30:13 +0530

    Add New File

---
+++ p2.java
@@ -0,0 +1,14 @@
+public class p2{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+
+        if(a > b){
+            System.out.println(a);
+        }else if(a < b){
+            System.out.println(b);
+        }else{
+            System.out.println("Both are equal");
+        }
+    }
+}
