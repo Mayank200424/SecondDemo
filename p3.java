f0d8eda973b4d38dc0d35cc96109314736e4a289
Mayank Busa <mayankbusa111@gamil.com>
2025-05-23 09:58:19 +0530

    M

--- p3.java
+++ p3.java
@@ -1,27 +1,27 @@
 public class p3{
     public static void main(String[] args) {
-        System.out.println("For Loop : ");
+        System.out.println("Reverse For Loop : ");
 
-        for(int i = 1; i <= 10; i++){
+        for(int i = 10; i >= 1; i--){
             System.out.println("Value of i is : " + i);
         }
 
         System.out.println("While Loop : ");
 
-        int i = 1;
+        int i = 10;
 
-        while(i <= 10){
+        while(i >= 1){
             System.out.println("Value of i is : " + i);
-            i++;
+            i--;
         }
 
         System.out.println("Do While Loop : ");
-        int a = 1;
+        int a = 10;
 
         do{
             System.out.println("Value of a is : " + a);
-            a++;
-        }while(a <= 10);
+            a--;
+        }while(a >= 1);
 
     }
 }
