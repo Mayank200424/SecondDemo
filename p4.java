811c1adbf5869aecec214a25116bd84a21607c12
Mayank Busa <mayankbusa111@gamil.com>
2025-05-23 10:08:55 +0530

    Modify

---
+++ p4.java
@@ -0,0 +1,9 @@
+public class p4{
+    public static void main(String[] args) {
+        System.out.println("Multiplcation Table : ");
+
+        for(int i = 5; i <= 50; i=i+5){
+            System.out.println(i);
+        }
+    }
+}
